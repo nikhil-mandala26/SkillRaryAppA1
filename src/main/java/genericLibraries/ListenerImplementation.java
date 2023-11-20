a4ab940a113b42ca7deb4139f4b10928460060cf
260968 - 5FIN <260968 - 5FIN@LAPTOP-TT56SPU4>
2023-11-20 12:46:16 +0530

    removed spaces in ListenerImplementation

--- src/main/java/genericLibraries/ListenerImplementation.java
+++ src/main/java/genericLibraries/ListenerImplementation.java
@@ -4,7 +4,7 @@ import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 
-public class ListenerImplementation   implements ITestListener{
+public class ListenerImplementation implements ITestListener{
 
 	@Override
 	public void onTestStart(ITestResult result) {
